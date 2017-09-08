2f6d467f4396715adabbb65b13a645b7c04bb72c
nazmul202101 <sheikhnazmulislam@gmail.com>
2017-09-08 21:18:25 +0600

    My first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+anime({
+    targets: 'div.box.red',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 0, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+   
+  });
+
+  anime({
+    targets: 'div.box.blue',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 100, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
+  anime({
+    targets: 'div.box.green',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 200, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
+
+  anime({
+    targets: 'div.box.yellow',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 300, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
+
+  anime({
+    targets: 'div.box.black',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 400, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
+
+  anime({
+    targets: 'div.box.purple',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 500, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
+
+
+  anime({
+    targets: 'div.box.magenta',
+    translateY: [
+      { value: 600, duration: 500 },
+      { value: 600, duration: 100 }
+    ],
+    rotate: '1000turn',
+    duration: 5000,
+    
+   
+  });
